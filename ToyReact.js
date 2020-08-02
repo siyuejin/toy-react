6c90ba2a3c046ca8d80713e0a2321fe521a27a64
siyuejin <siyuejin@hotmail.com>
2020-08-02 04:40:34 -0400

    Fix the issue introduced bc/ of typeof null === 'object' in JS

--- ToyReact.js
+++ ToyReact.js
@@ -92,7 +92,7 @@ export class Component {
     setState(state) {
         let merge = (oldState, newState) => {
             for (let p in newState) {
-                if (typeof newState[p] === 'object') {
+                if (typeof newState[p] === 'object' && newState[p] !== null) {
                     if (typeof oldState[p] !== 'object') {
                         oldState[p] = {};
                     }
