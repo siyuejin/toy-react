b691f4f2daa00cd1656842a23aaf5ef2e62638bb
siyuejin <siyuejin@hotmail.com>
2020-08-02 00:25:38 -0400

    Init code for Tic Tac Toe

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 import {ToyReact, Component} from './ToyReact';
-class MyComponent extends Component {
+class Square extends Component {
     render() {
-        return <div>
-            <span>hello</span>
-            <span>world!</span>
-            <div>
-                {true}
-                {this.children}
-            </div>
-        </div>
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
     }
 }
 
-let a = <MyComponent name="a" id="ida">
-    <div>123</div>
-</MyComponent>
+class Board extends Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+let a = <Board />
 
 
 ToyReact.render(
